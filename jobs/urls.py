8ee665340644504a02bf6d738c58136b741464cd
NinjaKoala21 <stewartjiang436@gmail.com>
2026-09-25 10:51:32 -0400

    Added recruiter job posting and editing.

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='jobs.index'),
+    path('mine/', views.my_jobs, name='jobs.mine'),
+    path('create/', views.create_job, name='jobs.create'),
+    path('<int:job_id>/edit/', views.edit_job, name='jobs.edit'),
 ]
